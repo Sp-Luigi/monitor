956a8784579c9e84c17f2044b977bc65733f3339
zard <467540052@qq.com>
2022-03-17 23:24:55 +0800

    webpack config change

--- packages/monitor-dom/webpack.config.js
+++ packages/monitor-dom/webpack.config.js
@@ -1,12 +1,16 @@
 const path = require('path');
 module.exports = {
   entry: path.join(__dirname,'./src/index.js'),
-  mode: "production",
+  mode: "development",
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].min.js'
+    filename: '[name].min.js',
+    library: {
+      name: 'monitor-dom',
+      type: 'umd',
+    },
   },
   optimization:{
     usedExports: false,
-  }
+  },
 }
